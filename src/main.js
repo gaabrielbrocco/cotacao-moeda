1e9662f01ffd59f07419234539bf1968416622de
Gabriel <gabriel@webcgi.com.br>
2024-03-20 08:58:20 -0300

    Remove cids

--- src/main.js
+++ src/main.js
@@ -3,9 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 import { vMaska } from "maska";
-import cids from "cids-cgi/lib";
 
 const app = createApp(App);
 
 app.directive("maska", vMaska);
-app.use(router).use(vuetify).use(cids, opt).mount("#app");
+app.use(router).use(vuetify).mount("#app");
